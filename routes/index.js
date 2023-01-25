924dfbf043b45fb3bd2ca770a8626f7800e56da9
Feras Jobeir <fjobeir@gmail.com>
2023-01-25 10:24:11 +0300

    setting test env

--- routes/index.js
+++ routes/index.js
@@ -4,23 +4,14 @@ const models = require("../models");
 const permission = require('../models/permission');
 
 /* GET home page. */
-router.get(
-  '/',
-  // isAuthenticated,
-  function(req, res, next) {
-    if (req.user.can('company:approve')) {
-      return next()
-    }
-    // return error
-  },
-  
-  async (req, res, next)=> {
-  const user = await models.User.findByPk(1);
-  if (await user.can("company:approve")) {
-    return next()
-  }
-  return res.send("done");
-  });
+router.get('/', function(req, res, next) {
+  res.status(200);
+  res.send({
+    success: true,
+    data: null,
+    messages: []
+  })
+});
 
   
 module.exports = router;
