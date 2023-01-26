7b520483bb66fdf63db93effc614fa5cae18f054
shafeeqhawa <shafeeq1996@hotmail.com>
2023-01-26 09:55:45 +0300

    prepare equipment

---
+++ controllers/equipmentController.js
@@ -0,0 +1,18 @@
+const models = require('../models')
+
+
+const store = async(req,res,next)=>{
+const result = {
+    success: true,
+    data: null,
+    messages: []
+}
+
+
+
+
+}
+
+module.exports={
+    store
+}
