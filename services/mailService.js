de39dfb0684e397066a52d24ae1f471bb0b4f3b9
shafeeqhawa <shafeeq1996@hotmail.com>
2023-02-15 10:52:35 +0300

    mail service

--- services/mailService.js
+++ services/mailService.js
@@ -3,26 +3,47 @@ var nodemailer = require('nodemailer');
 var transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
-    user: 'youremail@gmail.com',
-    pass: 'yourpassword'
+    user: 'toptepespor@gmail.com',
+    pass: 'vohluzkmhuxwquiz'
   }
 });
 
-var mailOptions = {
-  from: 'youremail@gmail.com',
-  to: 'myfriend@yahoo.com',
-  subject: 'Sending Email using Node.js',
-  text: 'That was easy!'
-};
-
-transporter.sendMail(mailOptions, function(error, info){
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('Email sent: ' + info.response);
+const mailTemplates = {
+  approveEmail: {
+    message: "Hello :name <br> Your account has been approved",
+    subject: "Account Activation"
+  },
+  reservationCancellation: {
+    message: "Hello :name <br> Your reservation on :date for :field has been cancelled",
+    subject: "Reservation has been cancelled"
   }
-});
+}
 
+// sendEmail(user, "reservationCancellation", {
+//   date: "2023-01-01",
+//   field: "Stad Alarab"
+// })
 
+function sendEmail(user, mailType, variables = {}) {
+  let message = mailTemplates[mailType].message
+  message.replace(':name', user.name)
+  Object.keys(variables).forEach((variable) => {
+    message.replace(':' + variable, variables[variable])
+  })
+  var mailOptions = {
+    from: 'toptepespor@gmail.com',
+    to: user.email,
+    subject: mailTemplates[mailType].subject,
+    text: message
+  };
+  
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
+}
 
-//const mailTemplates = 
+module.exports = { sendEmail }
